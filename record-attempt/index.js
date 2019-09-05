cd90538042013e006afc4acda1d13addd8c72a05
Llynch1998 <llynch1998@gmail.com>
2019-09-05 08:48:22 -0400

    record attempts 1

--- record-attempt/index.js
+++ record-attempt/index.js
@@ -1,14 +1,54 @@
 let mediaRecorder;
-let audioChunks;
+let audioChunks = [];
 let audioBlob;
 let audioUrls = [];
 let audioUrl;
 let audio;
+let recording = false;
 
 let recordBtn = document.querySelector("#record");
+
 let stopBtn = document.querySelector("#stop");
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    console.log('getUserMedia supported.');
+    navigator.mediaDevices.getUserMedia (
+       // constraints - only audio needed for this app
+       {
+          audio: true
+       })
+ 
+       // Success callback
+       .then(function(stream) {
+  
+         
+       })
+ 
+       // Error callback
+       .catch(function(err) {
+          console.log('The following getUserMedia error occured: ' + err);
+       }
+    );
+ } else {
+    console.log('getUserMedia not supported on your browser!');
+ }
+
+let mediaRecorder = new mediaRecorder(stream);
+
 recordBtn.onclick = function(e) {
-    
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
+}
+
+mediaRecorder.ondataavailable = function(e) {
+    audioChunks.push(e.data);
+  }
+
+stopBtn.onclick = function(){
+    mediaRecorder.stop();
+    console.log(mediaRecorder.state);
+    console.log("recorder stopped");
 }
 
 //This is the base record time out example.
@@ -33,3 +73,19 @@ recordBtn.onclick = function(e) {
 //         mediaRecorder.stop();
 //       }, 1000);
 //   });
+
+// recording = !recording;
+//     if(recording){
+//         mediaRecorder.start();
+//     }
+//     else if(recording == false){
+//         mediaRecorder.addEventListener("stop", () => {
+//             audioBlob = new Blob(audioChunks);
+//             audioUrl = URL.createObjectURL(audioBlob);
+//             audioUrls.push(audioUrl);
+//             audio = new Audio(audioUrl);
+//           audio.play();
+//           });
+
+//         mediaRecorder.stop();
+//     }
